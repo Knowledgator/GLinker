6ada78a78542fb4ac5c6043960d42cbbf23d9d61
BioMikeUkr <mykhayloshtopko@gmail.com>
2026-01-28 17:13:38 +0200

    init fixed

--- src/glinker/__init__.py
+++ src/glinker/__init__.py
@@ -5,13 +5,11 @@ A modular 4-layer entity linking pipeline using spaCy NER, database search, and 
 
 __version__ = "0.1.0"
 
-# Import processors to register them (decorators will execute)
 from glinker.l0 import processor as _l0_processor
 from glinker.l1 import processor as _l1_processor
 from glinker.l2 import processor as _l2_processor
 from glinker.l3 import processor as _l3_processor
 
-# Import core
 from glinker.core import (
     BaseConfig,
     BaseInput,
